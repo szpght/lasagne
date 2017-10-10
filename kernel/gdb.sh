9270ea78e7c8ef09871fa978dfb8028ddef2c078
Paweł Pietrasz <szpght@gmail.com>
2017-10-10 19:16:58 +0200

    set assembly syntax to intel in gdb

--- kernel/gdb.sh
+++ kernel/gdb.sh
@@ -5,5 +5,5 @@ qemu-system-x86_64 -s -S -cdrom $ISO_FILE &
 QEMU_PID=$!
 gdb $ELF_FILE -batch -ex 'target remote localhost:1234' -ex 'b initialize' -ex 'c' -ex 'disconnect'
 #gdb `find . -type d -printf '-d %p '` $ELF_FILE -ex 'set arch i386:x86-64' -ex 'target remote localhost:1234'
-gdb $ELF_FILE -ex 'set arch i386:x86-64' -ex 'target remote localhost:1234' -ex 'tui enable' -ex 'layout src'
+gdb $ELF_FILE -ex 'set arch i386:x86-64:intel' -ex 'set disassembly-flavor intel' -ex 'target remote localhost:1234' -ex 'tui enable' -ex 'layout src'
 kill $QEMU_PID

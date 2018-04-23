4c994699426e295786bfcda5976fed1a2874b020
Paweł Pietrasz <szpght@gmail.com>
2018-04-23 18:48:03 +0200

    lang: basic working grammar

--- language/Compiler/Compiler/Lasagne.g4
+++ language/Compiler/Compiler/Lasagne.g4
@@ -1,28 +1,45 @@
 ﻿grammar Lasagne;
 
-@parser::members
-{
-    protected const int EOF = Eof;
-}
- 
-@lexer::members
-{
-    protected const int EOF = Eof;
-    protected const int HIDDEN = Hidden;
-}
- 
-/*
- * Parser Rules
- */
- 
-compileUnit
-    :   EOF
-    ;
- 
-/*
- * Lexer Rules
- */
- 
-WS
-    :   ' ' -> channel(HIDDEN)
-    ;
+
+
+program : topLevelStatement* EOF;
+
+topLevelStatement : functionBlock;
+
+functionBlock : Def name paramList block;
+
+block : OpenBrace statement* CloseBrace;
+
+statement : assignStatement;
+
+assignStatement : lvalue AssignOperator rvalue;
+
+rvalue : name | literal;
+lvalue : name;
+
+literal : IntLiteral;
+
+paramList : OpenParen 
+    paramDecls
+    CloseParen;
+
+paramDecls : (name Colon type (Comma paramDecls)*)?;
+
+type : name;
+
+Def: 'def';
+
+name : ID;
+
+
+
+AssignOperator : '=';
+IntLiteral : ('0'..'9')+;
+Colon : ':';
+OpenBrace : '{';
+CloseBrace: '}';
+OpenParen : '(';
+CloseParen : ')';
+Comma: ',';
+ID : [a-zA-Z] [a-zA-Z0-9]*;
+WS : [ \t\r\n]+ -> skip ;
--- language/Compiler/Compiler/LasagneCompiler.cs
+++ language/Compiler/Compiler/LasagneCompiler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using Antlr4.Runtime;
 
 namespace Compiler
 {
@@ -7,6 +9,48 @@ namespace Compiler
         public void Compile()
         {
             Console.WriteLine("Compiling");
+            var text = "def hello () { question = 1 answer = 42 }";
+            var input = new AntlrInputStream(text);
+            var lexer = new LasagneLexer(input);
+            var commonTokenStream = new CommonTokenStream(lexer);
+            var parser = new LasagneParser(commonTokenStream);
+            var visitor = new LasagneBaseVisitor<string>();
+            var tree = parser.program();
+            var ctx = tree.ToStringTree(parser);
+            Console.WriteLine(FormatSexp(ctx));
+         }
+
+        public string FormatSexp(string sexp)
+        {
+            var indent = 0;
+            const int step = 2;
+            var output = new StringBuilder();
+            for (int i = 0; i < sexp.Length; ++i)
+            {
+                var ch = sexp[i];
+                if (ch == '(')
+                {
+                    output.Append("\n");
+                    for (int j = 0; j < indent; ++j)
+                    {
+                        output.Append(' ');
+                    }
+
+                    output.Append('(');
+                    indent += step;
+                }
+                else if (ch == ')')
+                {
+                    indent -= step;
+                    output.Append(')');
+                }
+                else
+                {
+                    output.Append(ch);
+                }
+            }
+
+            return output.ToString();
         }
     }
 }
